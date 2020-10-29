beb82aaf407fe3909422e6b8e8f095c2560cffd4
Michel Blancard <github@michelblancard.fr>
2020-10-29 09:29:33 +0100

    Td5

---
+++ td5.py
@@ -0,0 +1,19 @@
+# Pour une liste de nombres, comment calculer la somme des nombres ?
+
+
+
+
+# Et le produit des nombres ?
+
+liste_des_nombre_a_multiplier = [5, 2, 7, 2, 5, 6]
+resultat_intermediaire = 1
+for compteur in liste_des_nombre_a_multiplier:
+    resultat_intermediaire = resultat_intermediaire * compteur
+resultat_final = resultat_intermediaire
+print(resultat_final)
+
+
+
+# Déterminer le maximum et le minimum ?
+
+
