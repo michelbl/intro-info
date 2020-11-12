3c8ad53cd4e59bd10982460f2c71ae6965b6f2c0
Michel Blancard <github@michelblancard.fr>
2020-11-12 13:02:46 +0100

    Fonctions de codage/décodage

--- cesar.py
+++ cesar.py
@@ -6,11 +6,51 @@ DWWDTXHUDOHVLDPDLQWHQDQW
 ATTAQUERALESIAMAINTENANT
 """
 
-lettre = "E"
-codage_numerique = ord(lettre)-64
-codage_numerique_apres_decalage = codage_numerique + 3
+def chiffre_une_lettre(lettre):
+    #print("Lettre :", lettre)
 
-print("E" + 3)
+    codage_numerique = ord(lettre)-64
 
-nombre = 1
-print("Lettre correspondante :", chr(nombre+64))
+    #print("Nombre correspondant :", codage_numerique)
+
+    codage_numerique_apres_decalage = codage_numerique + 3
+    if codage_numerique_apres_decalage > 26:
+        codage_numerique_apres_decalage = codage_numerique_apres_decalage - 26
+
+    #print('Code de la lettre encryptée', codage_numerique_apres_decalage)
+
+    lettre_apres_decalage = chr(codage_numerique_apres_decalage+64)
+
+    #print("Lettre codée :", lettre_apres_decalage)
+
+    return lettre_apres_decalage
+
+
+def dechiffre_une_lettre(lettre):
+    #print("Lettre :", lettre)
+
+    codage_numerique = ord(lettre)-64
+
+    #print("Nombre correspondant :", codage_numerique)
+
+    codage_numerique_apres_decalage = codage_numerique - 3
+    if codage_numerique_apres_decalage < 0:
+        codage_numerique_apres_decalage = codage_numerique_apres_decalage + 26
+
+    #print('Code de la lettre décryptée', codage_numerique_apres_decalage)
+
+    lettre_apres_decalage = chr(codage_numerique_apres_decalage+64)
+
+    #print("Lettre décodée :", lettre_apres_decalage)
+
+    return lettre_apres_decalage
+
+
+
+lettre_encodee = chiffre_une_lettre(lettre="A")
+print("Resultat du codage de la lettre A :", lettre_encodee)
+
+print()
+
+lettre_decodee = dechiffre_une_lettre(lettre="A")
+print("Resultat du decodage de la lettre A :", lettre_decodee)
