71b7cc7d6d92184c333de9855cba7bd9702713f4
Michel Blancard <github@michelblancard.fr>
2020-11-12 11:34:37 +0100

    Codage et décodage d'une lettre

--- cesar.py
+++ cesar.py
@@ -1,7 +1,16 @@
 
-BONJOUR MONDE
+"""BONJOUR MONDE
 ERQMRXU PRQGH
 
 DWWDTXHUDOHVLDPDLQWHQDQW
 ATTAQUERALESIAMAINTENANT
+"""
 
+lettre = "E"
+codage_numerique = ord(lettre)-64
+codage_numerique_apres_decalage = codage_numerique + 3
+
+print("E" + 3)
+
+nombre = 1
+print("Lettre correspondante :", chr(nombre+64))
