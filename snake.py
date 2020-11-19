d52030b9f436b3ab568126a57bbf0d0a40931ca6
Michel Blancard <github@michelblancard.fr>
2020-11-19 11:09:25 +0100

    Snake

---
+++ snake.py
@@ -0,0 +1,43 @@
+import time
+import os
+
+os.system('clear')
+
+print('--1-------')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('---1------')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('----1-----')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('-----1----')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('------1---')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('-------1--')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('--------1-')
+
+time.sleep(0.5)
+os.system('clear')
+
+print('---------1')
+
+print('Le jeu est fini')
